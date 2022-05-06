30cde190ba207ff77d559c7224c9e3ecf8f11aeb
Siddhant kudal <85402561+siddhantkudal@users.noreply.github.com>
2022-05-06 17:13:05 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,9 @@ class HelloWorld{
 	public static void main(String []args){
 
 	System.out.print("Hello World ");
+	System.out.print("siddhant kUdal ");
+	System.out.print("walchand institute of technology solapur ");
+	System.out.print("Solapur ");
 	
  	}
 
